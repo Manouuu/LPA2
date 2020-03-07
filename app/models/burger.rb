18b4e6cd0b79310870b386c5e290e9182ac3a814
Emmanuel LACROIX <emmanuellacroix@mbp-de-emmanuel.home>
2020-03-07 22:01:59 +0100

    Fix undefined method nutriments for nil:NilClass V2

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -3,19 +3,20 @@ class Burger < ApplicationRecord
     def nutriments
         code = self.code
         @product = Openfoodfacts::Product.get(code, locale: 'fr')
-        @nutriments = @product.nutriments
-        @resultset = [
-            "salt" => @nutriments.salt,
-            "salt_unit" => @nutriments.salt_unit, 
-            "sugars" => @nutriments.sugars,
-            "sugars_unit" => @nutriments.sugars_unit,
-            "proteins" => @nutriments.proteins,
-            "proteins_unit" => @nutriments.proteins_unit,
-            "sodium" => @nutriments.sodium,
-            "sodium_unit" => @nutriments.sodium_unit,
-            'energy' => @nutriments.energy,
-            'energy_unit' => @nutriments.energy_unit
-        ]
+        if(@product)
+            @nutriments = @product.nutriments
+            @resultset = [
+                "salt" => @nutriments.salt,
+                "salt_unit" => @nutriments.salt_unit, 
+                "sugars" => @nutriments.sugars,
+                "sugars_unit" => @nutriments.sugars_unit,
+                "proteins" => @nutriments.proteins,
+                "proteins_unit" => @nutriments.proteins_unit,
+                "sodium" => @nutriments.sodium,
+                "sodium_unit" => @nutriments.sodium_unit,
+                'energy' => @nutriments.energy,
+                'energy_unit' => @nutriments.energy_unit
+            ]
         #return product.nutriments.to_hash
     end
 end
