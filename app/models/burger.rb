ca0a91e9efdfe328023ba5c20f198b48f5ed320b
Emmanuel LACROIX <emmanuellacroix@mbp-de-emmanuel.home>
2020-03-07 21:47:40 +0100

    Display 5 nutriments

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -2,7 +2,19 @@ class Burger < ApplicationRecord
     #Call openfoodfacts API
     def nutriments
         code = self.code
-        product = Openfoodfacts::Product.get(code, locale: 'fr')
-        return product.nutriments.to_hash
+        @nutriments = Openfoodfacts::Product.get(code, locale: 'fr').nutriments
+        @resultset = [
+            "salt" => @nutriments.salt,
+            "salt_unit" => @nutriments.salt_unit, 
+            "sugars" => @nutriments.sugars,
+            "sugars_unit" => @nutriments.sugars_unit,
+            "proteins" => @nutriments.proteins,
+            "proteins_unit" => @nutriments.proteins_unit,
+            "sodium" => @nutriments.sodium,
+            "sodium_unit" => @nutriments.sodium_unit,
+            'energy' => @nutriments.energy,
+            'energy_unit' => @nutriments.energy_unit
+        ]
+        #return product.nutriments.to_hash
     end
 end
